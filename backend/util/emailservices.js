11a2a39d93fa532a64911b9c760db2a085f15dbb
officialsonia1018-lab <officialsonia.1018@gmail.com>
2026-06-10 13:20:27 +0530

    Update emailservices.js bravo email and password

--- backend/util/emailservices.js
+++ backend/util/emailservices.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer");
 
+console.log("BREVO_USER:", process.env.BREVO_USER);
+console.log("BREVO_PASS exists:", !!process.env.BREVO_PASS);
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS,
