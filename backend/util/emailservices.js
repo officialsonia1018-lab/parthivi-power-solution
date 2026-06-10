32b8ee0f8109bb65f18d4c5f07e050f40c973cdd
officialsonia1018-lab <officialsonia.1018@gmail.com>
2026-06-10 11:33:21 +0530

    Update emailservices.js

--- backend/util/emailservices.js
+++ backend/util/emailservices.js
@@ -2,15 +2,16 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-transporter.verify((error, success) => {
+transporter.verify((error) => {
   if (error) {
     console.log("SMTP ERROR:", error);
   } else {
